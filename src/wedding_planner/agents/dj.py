bf27a37767908cc280c4f6916c861f7540218e31
DapaMassimo <massimo.dapa96@gmail.com>
2026-04-09 22:45:33 +0200

    dj agent

---
+++ src/wedding_planner/agents/dj.py
@@ -0,0 +1,11 @@
+from wedding_planner.agents.base import Agent
+from wedding_planner.prompts_loader import DJ_SYSTEM_PROMPT
+from wedding_planner.config import DEFAULT_MODEL
+
+DJ_AGENT = Agent(
+    name="dj",
+    description="Queries music DB looking for a wedding appropriate playlist",
+    system_promp=DJ_SYSTEM_PROMPT,
+    model=DEFAULT_MODEL,
+    tools=[]
+)
